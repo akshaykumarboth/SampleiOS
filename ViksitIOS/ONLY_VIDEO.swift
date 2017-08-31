02689c728d371f46ef6ba041bbc76cc29ac13ff1
“akshayISTAR“ <akshay@istarindia.com>
2017-08-31 19:45:30 +0530

    assessment vc scene added

--- ViksitIOS/ONLY_VIDEO.swift
+++ ViksitIOS/ONLY_VIDEO.swift
@@ -20,23 +20,23 @@ class ONLY_VIDEO: UIViewController {
     
     @IBAction func startVideoPressed(_ sender: UIButton) {
         
-        //playVideoAV(urlString: "http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_1mb.mp4")
-        playVideoAV(urlString: slide.video.url)
+        playVideoAV(urlString: "http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_1mb.mp4")
+        //playVideoAV(urlString: slide.video.url)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        //getVideoFromDocuments(videoURL: "http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_1mb.mp4")
-        getVideoFromDocuments(videoURL: slide.video.url)
+        getVideoFromDocuments(videoURL: "http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_1mb.mp4")
+        //getVideoFromDocuments(videoURL: slide.video.url)
         
     }
 
+    // play video
     func playVideoAV(urlString: String) {
         
         let url = getVideoFromDocuments(videoURL: urlString)
         let player = AVPlayer(url: url)
-        //
         let av = AVPlayerViewController()
         av.player = player
         av.view.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.height)
@@ -47,6 +47,7 @@ class ONLY_VIDEO: UIViewController {
         
     }
     
+    //getting file from document directory
     func getVideoFromDocuments(videoURL: String) -> URL{
         let finalFileName = videoURL.components(separatedBy: "/").last
         print(finalFileName!)
@@ -62,6 +63,7 @@ class ONLY_VIDEO: UIViewController {
         return fooURL
     }
     
+    //saving video asynchronously in document directory
     func saveVideoAsync(urlToYourVideo: String) {
         //let videoImageUrl = "http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_1mb.mp4"
         
