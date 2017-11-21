5d965341412bac1069f087d8e3ba883be9d26935
Akshay Kumar Both <akshaykumarboth@gmail.com>
2017-11-21 23:36:01 +0530

    Update DataCache.swift

--- SampleiOS/DataCache.swift
+++ SampleiOS/DataCache.swift
@@ -1,10 +1,5 @@
 //
 //  DataCache.swift
-//  HttpTest
-//
-//  Created by Istar Feroz on 18/07/17.
-//  Copyright © 2017 Istar Feroz. All rights reserved.
-//
 
 import Foundation
 class DataCache: NSObject {
