4dca3bd855298634e571d249e37e8a9191e10055
Akshay Kumar Both <akshaykumarboth@gmail.com>
2017-11-21 21:58:56 +0530

    Update CMSMedia.swift

--- SampleiOS/CMSMedia.swift
+++ SampleiOS/CMSMedia.swift
@@ -1,10 +1,5 @@
 //
 //  CMSImage.swift
-//  ViksitIOS
-//
-//  Created by Akshay Kumar Both on 8/22/17.
-//  Copyright © 2017 Istar Feroz. All rights reserved.
-//
 
 import Foundation
 import SWXMLHash
